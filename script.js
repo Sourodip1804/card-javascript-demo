86cfc4f374ef00ceb0d4868da54550820bf32311
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-20 10:04:01 +0530

    modifying the file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // to make this createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
 
 
-function CreateCard()
+function CreateCard(title, cName, views, monthsOld, duration, thumbnail)
